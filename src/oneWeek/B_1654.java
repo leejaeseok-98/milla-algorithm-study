ee6a77c292822a1bb00d17ec1c7ac23919e55c13
leejaeseok-98 <wotjrdl98@naver.com>
2025-08-09 11:40:42 +0900

    좌표압축

---
+++ src/oneWeek/B_1654.java
@@ -0,0 +1,47 @@
+package oneWeek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(binarySearch(arr, m));
+    }
+
+    private static Long binarySearch(int[] arr, int m){
+        long low = 1;
+        long high = arr[arr.length - 1];
+        long result = 0L;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long count = 0L;
+            for (int i = 0; i < arr.length; i++) {
+                count += (arr[i] / mid);
+            }
+
+            if (count >= m) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/oneWeek/B_18870.java
@@ -0,0 +1,40 @@
+package oneWeek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class B_18870{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(arr);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], index);
+                ++index;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(arrCopy[i])).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
