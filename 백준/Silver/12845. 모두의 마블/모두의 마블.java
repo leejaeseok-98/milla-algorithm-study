8aeb42633761d1fc2cca5a631728f6d8f4dad978
leejaeseok-98 <wotjrdl98@naver.com>
2025-08-22 13:51:00 +0900

    [Silver III] Title: 모두의 마블, Time: 116 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = 0;
+        int[] arr = new int[T];
+        for (int i = 0; i < T; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < T - 1; i++) {
+            result += arr[T - 1] + arr[i];
+            arr[i] = Math.max(arr[i], arr[T - 1]);
+        }
+        System.out.println(result);
+    }
+}
