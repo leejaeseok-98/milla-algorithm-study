d7e336ba43a5cfa4a94ae5add929556d7cdd3328
leejaeseok-98 <wotjrdl98@naver.com>
2025-12-08 12:11:49 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2220 ms, Memory: 363608 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int[] arr = new int[T];
+        for (int i = 0; i < T; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0;i<T;i++){
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
