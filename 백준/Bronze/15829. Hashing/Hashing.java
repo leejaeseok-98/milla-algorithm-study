3ee51f1d84c5c12d9324eb0e70828b98455e7bd1
leejaeseok-98 <wotjrdl98@naver.com>
2026-01-05 13:56:23 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,27 +4,15 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int r = 31;
-        int m = 1234567891;
-
-        int l = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        long result = 0;
-
-        char[] a = new char[l];
-        a = str.toCharArray();
-
-        int[] arr = new int[l];
-        for (int i = 0; i < l; i++) {
-            arr[i] = a[i] - '`';
-        }
-        for (int i = 0; i < l; i++) {
-            result += (long) (arr[i] * Math.pow(r,i));
-        }
-
-        System.out.println(result % m);
-
-
-    }
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < L; i++) {
+			result += ((S.charAt(i) - 96) * pow);
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(result % 1234567891);
+	}
 }
